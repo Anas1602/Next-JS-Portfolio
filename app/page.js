ed2649c941e190cec86e967e41b2ee6cd0760cdf
Anas LARIBI <anas.laribi95@gmail.com>
2025-03-02 03:53:06 -0500

    Header Added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
+import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Header />
     </>
   );
 }
