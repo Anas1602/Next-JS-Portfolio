89968655786a94c2d2b1239a34c853ff470e55d3
Anas LARIBI <anas.laribi95@gmail.com>
2025-03-02 03:35:33 -0500

    Navigation Bar Completed

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,12 +1,24 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 
 const NavBar = () => {
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
+
   return (
     <>
+      <div className="fixed top0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
-        <a href="">
+        <a href="top">
           <Image
             alt=""
             src={assets.logo}
@@ -14,7 +26,7 @@ const NavBar = () => {
           />
         </a>
 
-        <ul>
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
           <li>
             <a href="#top">Home</a>
           </li>
@@ -32,11 +44,60 @@ const NavBar = () => {
           </li>
         </ul>
 
-        <div>
-          <a href="#contact">
+        <div className="flex items-center gap-4">
+          <button>
+            <Image src={assets.moon_icon} alt="" className="w-6" />
+          </button>
+
+          <a
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+            href="#contact">
             Contact <Image alt="" src={assets.arrow_icon} className="w-3" />
           </a>
+
+          <button onClick={openMenu} className="block md:hidden ml-3">
+            <Image src={assets.menu_black} alt="" className="w-6 " />
+          </button>
         </div>
+
+        {/* Mobile Menu */}
+        <ul
+          ref={sideMenuRef}
+          className="md:hidden flex flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="cursor-pointer w-5"
+            />
+          </div>
+
+          <li>
+            <a onClick={closeMenu} href="#top">
+              Home
+            </a>
+          </li>
+          <li>
+            <a onClick={closeMenu} href="#about">
+              About me
+            </a>
+          </li>
+          <li>
+            <a onClick={closeMenu} href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a onClick={closeMenu} href="#work">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a onClick={closeMenu} href="#contact">
+              Contact Me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
