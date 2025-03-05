0ffbf8d3f59aae78998402bed84ffa44f6627fba
Anas LARIBI <anas.laribi95@gmail.com>
2025-03-05 04:37:09 -0500

    Animation added to About + Services Sections!

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,53 +1,99 @@
 import React from "react";
 import Image from "next/image";
 import { assets, infoList, toolsData } from "@/assets/assets";
+import { motion } from "motion/react";
 
 const About = () => {
   return (
-    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20 ">
-      <h4 className="text-center mb-2 text-lg">Introduction</h4>
-      <h2 className="text-center text-5xl">About Me</h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+      id="about"
+      className="w-full px-[12%] py-10 scroll-mt-20 ">
+      <motion.h4
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center mb-2 text-lg">
+        Introduction
+      </motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl">
+        About Me
+      </motion.h2>
 
-      <div className="flex w-full flex-col lg:flex-row items-center justify-center gap-20 my-20 max-w-7xl mx-auto">
-        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="flex w-full flex-col lg:flex-row items-center justify-center gap-20 my-20 max-w-7xl mx-auto">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6 }}
+          className="w-64 sm:w-80 rounded-3xl max-w-none">
           <Image
             src={assets.user_image}
             alt="user"
             className="w-full rounded-3xl"
           />
-        </div>
-        <div className="flex-1">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.8 }}
+          className="flex-1">
           <p className="mb-10 max-w-2xl">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur,
             optio! Odit harum consequatur velit necessitatibus perferendis
             tempore ipsa accusamus repudiandae, quibusdam, ratione incidunt,
             quasi assumenda aliquam impedit quidem quam magni?
           </p>
-          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1 }}
+            className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
             {infoList.map(({ icon, iconDark, title, description }, index) => (
-              <li
+              <motion.li
+                whileHover={{ scale: 1.05 }}
                 className="border-[.5px] border-gray-300 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-xl"
                 key={index}>
                 <Image className="w-7 mt-3" src={icon} alt={title} />
                 <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
                 <p className="text-sm text-gray-600"> {description} </p>
-              </li>
+              </motion.li>
             ))}
-          </ul>
+          </motion.ul>
 
-          <h4 className="my-6 text-gray-700">Tools I use</h4>
-          <ul className="flex items-center gap-3 sm:gap-5">
+          <motion.h4
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.3, delay: 0.5 }}
+            className="my-6 text-gray-700">
+            Tools I use
+          </motion.h4>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.5 }}
+            className="flex items-center gap-3 sm:gap-5">
             {toolsData.map((tool, index) => (
-              <li
+              <motion.li
+                whileInView={{ scale: 1.1 }}
                 key={index}
                 className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500">
                 <Image src={tool} alt="tool" className="w-5 sm:w-7" />
-              </li>
+              </motion.li>
             ))}
-          </ul>
-        </div>
-      </div>
-    </div>
+          </motion.ul>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
